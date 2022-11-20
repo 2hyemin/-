3c3e84aa1601ed7c1d094bbb14f9cbcf619b3147
2hyemin <110434511+2hyemin@users.noreply.github.com>
2022-11-20 17:52:50 +0900

    Add files via upload

---
+++ 03_example.py
@@ -0,0 +1,37 @@
+# Q01. 다음 코드의 결괏값은 무엇일까?
+# a = "Life is too short, you need python"
+
+# if "wife" in a :
+#     print("wife")
+# elif "python" in a and "you" not in a :
+#     print("ptyhon")
+# elif "shirt" not in a :
+#     print("shirt")
+# elif "need" in a :
+#     print("need")
+# else :
+#   print("none")
+
+
+# Q2. while 문을 사용하여 1부 1000까지의 자연수 중 3의 배의 합을 구해보자.
+# result = 0
+# i = 1
+# while i <= 1000 :
+#     if i % 3 == 0 :
+#         result += i
+#     i += 1
+# print(result)
+
+# Q3. while 문을 사용하여 다음과 같이 별(*)을 표시하는 프로그램을 작성해보자.
+i = 0
+while True:
+    i += 1
+    if i > 5: break
+    print ('*' * i)
+
+
+# Q4. for 문을 사용해 1부터 100까지의 숫자를 출력해보자.
+# for i in range(1,101):
+#     print(i)
+
+
